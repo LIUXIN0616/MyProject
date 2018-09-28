d8956eebae5a6b588a50574f222ca56618521af9
liuxin <liuxin@vv.cc>
2018-09-28 13:48:39 +0800

    添加MVParms框架

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.test.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author 036
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
